8ed4fd6e810135f9da878a5ab0a970c9dc1d173e
Induwara Mapalagama <induwara.2019779@iit.ac.lk>
2021-10-12 15:29:32 +0530

    added the header base

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { AppHeader } from "./components/app-header";
+
+
 
 
 ReactDOM.render(
+    
     <React.StrictMode>
-        
+        <AppHeader />
     </React.StrictMode>,
     document.getElementById("root")
 );
