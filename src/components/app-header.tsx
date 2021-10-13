d741834a491693b152eff83094546b9dfac97721
Induwara Mapalagama <induwara.2019779@iit.ac.lk>
2021-10-13 14:44:56 +0530

    updated the profile pic

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Button ,Avatar} from 'rsuite';
 import '../assets/styles/headerStyles.css'
 import 'rsuite/styles/index.less'; 
+import "rsuite/dist/rsuite.min.css";
 
 export const AppHeader : React.FC = () => {
     return(
-        <div className="header">
-            <div className="header-box">
-                <p>Hey there !</p>
+        <div>
+            <div className="header"></div>
+            <div className="pic">
+                <Avatar circle style={{background:'orange',height:100, width:100}}>profile</Avatar>
             </div>
-        </div>
-    
+            {/* <div className="header-box">
+                <p>Hey there !</p>
+            </div> */}
+        </div>  
     );
 };
