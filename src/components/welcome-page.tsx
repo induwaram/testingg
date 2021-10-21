1d5115ce015fa205c1b40c506da1b208fe4e869a
Induwara Mapalagama <induwara.2019779@iit.ac.lk>
2021-10-21 08:58:37 +0530

    Update welcome-page.tsx

--- src/components/welcome-page.tsx
+++ src/components/welcome-page.tsx
@@ -11,7 +11,7 @@ export const WelcomePage :React.FC =() =>{
                     <p>Hey There,  Welcome to Asgardeo Playground !</p>
                 </div>
             </div>
-            <p className="bodyText">You can visit <span style={{color:'#FF7300'}}>console</span> to make an account and try out the playground </p>
+            <p className="bodyText">You can visit <span style={ { color:'#FF7300' } }>console</span> to make an account and try out the playground </p>
         </div>
     ); 
 };
