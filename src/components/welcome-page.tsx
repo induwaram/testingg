6cc91edf0a2a8854f3d7e1545f630120d9b7b3c0
Induwara Mapalagama <induwara.2019779@iit.ac.lk>
2021-10-21 09:01:31 +0530

    updated welcome page

--- src/components/welcome-page.tsx
+++ src/components/welcome-page.tsx
@@ -1,7 +1,14 @@
-/* eslint-disable quotes */
-/* eslint-disable max-len */
-import React from 'react';
-import '../assets/styles/welcomePage.css';
+/**
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.com). All Rights Reserved.
+ *
+ * This software is the property of WSO2 Inc. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein in any form is strictly forbidden, unless permitted by WSO2 expressly.
+ * You may not alter or remove any copyright or other notice from copies of this content."
+ */
+
+import React from "react";
+import "../assets/styles/welcomePage.css";
 
 export const WelcomePage :React.FC =() =>{
     return(
@@ -11,6 +18,7 @@ export const WelcomePage :React.FC =() =>{
                     <p>Hey There,  Welcome to Asgardeo Playground !</p>
                 </div>
             </div>
+
             <p className="bodyText">You can visit <span style={ { color:'#FF7300' } }>console</span> to make an account and try out the playground </p>
         </div>
     ); 
